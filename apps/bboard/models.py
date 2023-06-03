1243a856adc18f0c46af9dd3fa774e36c89395be
whiteRR <whiterr2134@gmail.com>
2023-06-03 20:59:33 +0600

    added class Types and type field to class Board

--- apps/bboard/models.py
+++ apps/bboard/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.utils.translation import gettext
 
 
 class Board(models.Model):
+    class Types(models.TextChoices):
+        BUY = 'B', gettext("Куплю")
+        SELL = 'S', gettext("Продам")
+
     title = models.CharField(max_length=50, verbose_name='Тип товара')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
+    type = models.CharField(max_length=1, choices=Types.choices, default=Types.SELL, verbose_name='Тип обьявление')
     rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
 
     class Meta:
