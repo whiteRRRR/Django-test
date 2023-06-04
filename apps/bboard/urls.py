b5e8827eb17795da95140778c33ef59e58a7c8ff
whiteRR <whiterr2134@gmail.com>
2023-06-04 11:36:19 +0600

    replaced url route

--- apps/bboard/urls.py
+++ apps/bboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('', ByMainPageView.as_view(), name='index'),
+    path('<int:rubric_id>/', ByRubricView.as_view(), name='by_rubric'),
     path('add/', BbCreateView.as_view(), name='add')
 ]
