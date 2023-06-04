a185fe2fedf3d76831c1ed77a3df4f6e08e9f61b
whiteRR <whiterr2134@gmail.com>
2023-06-04 11:37:40 +0600

    function controller replaced with class controller

--- apps/bboard/views.py
+++ apps/bboard/views.py
@@ -1,24 +1,37 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 from django.urls import reverse_lazy  # Лучше использвать этот метод для перенаправления
 from .models import *
 from .forms import BoardForm
 
 
-def index(request):
-    bbs = Board.objects.all()
-    rubrics = Rubric.objects.all()
-    contex = {'bbs': bbs, 'rubrics': rubrics}
-    return render(request, 'bboard/index.html', contex)
+class ByMainPageView(ListView):
+    model = Board
+    template_name = 'bboard/index.html'
+    context_object_name = 'bbs'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 
-def by_rubric(request, rubric_id):
-    bbs = Board.objects.filter(rubric=rubric_id)
-    rubrics = Rubric.objects.all()
-    current_rubric = Rubric.objects.get(pk=rubric_id)
-    context = {'bbs': bbs, 'rubrics': rubrics,
-               'current_rubric': current_rubric}
-    return render(request, 'bboard/by_rubric.html', context)
+class ByRubricView(ListView):
+    model = Board
+    template_name = 'bboard/by_rubric.html'
+    context_object_name = 'bbs'
+
+    def get_queryset(self):
+        rubric_id = self.kwargs['rubric_id']
+        return Board.objects.filter(rubric=rubric_id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        rubric_id = self.kwargs['rubric_id']
+        context['rubrics'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(pk=rubric_id)
+        return context
 
 
 class BbCreateView(CreateView):
