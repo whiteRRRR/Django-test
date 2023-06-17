fad754374166d2dd5af0b84a3b3bfd060de9fc57
whiteRR <whiterr2134@gmail.com>
2023-06-17 12:50:56 +0600

    swapped

--- apps/bboard/urls.py
+++ apps/bboard/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', ByMainPageView.as_view(), name='index'),
+    path('add/', BbCreateView.as_view(), name='add'),
     path('<int:rubric_id>/', ByRubricView.as_view(), name='by_rubric'),
     path('<str:type_char>/', ByTypesBoard.as_view(), name='type_board'),
-    path('add/', BbCreateView.as_view(), name='add')
 ]
